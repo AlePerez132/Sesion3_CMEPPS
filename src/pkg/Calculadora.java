4902bc4f571fd9d664581bd1f071073b683b5172
alepd <alepd@PORTATIL-ALE>
2023-10-10 11:22:13 +0200

    .

--- src/pkg/Calculadora.java
+++ src/pkg/Calculadora.java
@@ -3,18 +3,22 @@ package pkg;
 public class Calculadora {
 	
 	public static Integer suma(int a, int b) {
-		return 5;
+		return a+b;
 	}
 	
 	public static Integer resta(int a, int b) {
-		return -1;
+		return a-b;
 	}
 	
 	public static Integer multiplica(int a, int b) {
-		return 6;
+		return a*b;
 	}
 	
 	public static Integer divide(int a, int b) {
-		return 0;
+		if(b!=0)
+			return a/b;
+		else
+			return -1;
 	}
+	
 }
