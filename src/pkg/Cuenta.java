f0fdf166882e05f6f4b55d5c984b19a5e2708c37
alepd <alepd@PORTATIL-ALE>
2023-10-11 12:12:08 +0200

    paso 3 (funciona)

--- src/pkg/Cuenta.java
+++ src/pkg/Cuenta.java
@@ -3,43 +3,43 @@ package pkg;
 public class Cuenta {
 	private double saldo;
 	private String nombre, titular;
-	
-	
+
 	public Cuenta(String nombre, String titular, double saldo) {
-		this.nombre=nombre;
-		this.titular=titular;
-		this.saldo=saldo;
-	} 
-	
+		this.nombre = nombre;
+		this.titular = titular;
+		this.saldo = saldo;
+	}
+
 	public void ingresar(double cantidad) {
-		this.saldo+=cantidad;
+		this.saldo += cantidad;
 	}
-	
+
 	public void retirar(double cantidad) {
-		if(cantidad <= this.saldo) 
-			this.saldo-=cantidad;
-		
+		if(this.saldo-cantidad >= -500) {
+			this.saldo -= cantidad;
+		}
 	}
+
 	public double getSaldo() {
 		return saldo;
 	}
-	
+
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
-	
+
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
+
 	public String getTitular() {
 		return titular;
 	}
-	
+
 	public void setTitular(String titular) {
 		this.titular = titular;
 	}
--- src/test/CuentaTest.java
+++ src/test/CuentaTest.java
@@ -15,8 +15,8 @@ class CuentaTest {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		aux =new Cuenta("12345", "Manuel", 50);
-		aux2 =new Cuenta("67890", "Javier", 0);
+		aux = new Cuenta("12345", "Manuel", 50);
+		aux2 = new Cuenta("67890", "Javier", 0);
 	}
 
 	@AfterAll
@@ -25,33 +25,35 @@ class CuentaTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		aux.setSaldo(0);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 	}
 
+	/*
+	 * @Test void testIngresar() { 
+	 * aux.ingresar(1000); 
+	 * assertEquals(aux.getSaldo(), 1000); 
+	 * }
+	 * 
+	 * @Test void testRetirar() { 
+	 * aux.retirar(1000); 
+	 * assertEquals(aux.getSaldo(), 0); 
+	 * }
+	 * 
+	 */
+
 	@Test
-	void testIngresar() {
-		aux.ingresar(1000);
-		assertEquals(aux.getSaldo(), 1000);
-	}
-	
-	@Test
-	void testRetirar() {
-		aux.retirar(1000);
-		assertEquals(aux.getSaldo(), 0);
-	}	
-	
-	@Test void operacionCuenta1() {
+	void operacionCuenta1() {
 		aux.retirar(200);
 		aux.ingresar(100);
 		aux.retirar(200);
 		assertEquals(-250, aux.getSaldo());
 	}
-	
-	@Test void operacionCuenta2() {
+
+	@Test
+	void operacionCuenta2() {
 		aux2.retirar(350);
 		aux2.retirar(150);
 		aux2.ingresar(50);
